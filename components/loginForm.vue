bea19302a44d4fcc99f69a7b12c610d5bb730f18
luoshuai990529 <1029113525@qq.com>
2020-08-23 14:15:48 +0800

    首页登录注册组件抽离，注册功能完成

---
+++ components/loginForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item">
+      <el-input placeholder="用户名/手机"></el-input>
+    </el-form-item>
+
+    <el-form-item class="form-item">
+      <el-input placeholder="密码" type="password"></el-input>
+    </el-form-item>
+
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
+
+    <el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button>
+  </el-form>
+</template>
+
+<script>
+/* 
+1. 在`components/user`中新建`loginForm.vue`表单组件
+2. 使用`Element-ui`的 [form](https://element.eleme.cn/#/zh-CN/component/form) 表单组件进行表单验证
+   1. 输入框失去焦点时,自动验证
+   2. 点击登录按钮时, 再次验证
+   3. 将 `token` 缓存到  `vuex`  中
+ */
+export default {
+  data() {
+    return {
+      // 表单数据
+      form: {},
+      // 表单规则
+      rules: {},
+    };
+  },
+  methods: {
+    // 提交登录
+    handleLoginSubmit() {
+      console.log(this.form);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,78 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- 幻灯片 -->
+    <div class="carousel">
+      <!-- 轮播图 -->
+      <el-carousel trigger="click" :interval="5000" arrow="hover">
+        <el-carousel-item v-for="(item, index) in banners" :key="index">
+          <div
+            class="banner-image"
+            :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+          ></div>
+        </el-carousel-item>
+      </el-carousel>
+
+      <div class="search_tab">
+        <search-tab></search-tab>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import SearchTab from "@/components/searchTab";
+export default {
+  components: {
+    SearchTab
+  },
+  data() {
+    return {
+      // 轮播图数据
+      banners: [
+        {
+          url: "http://157.122.54.189:9095/assets/images/th03.jfif",
+        },
+        {
+          url: "http://157.122.54.189:9095/assets/images/th04.jfif",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
+.carousel {
+  position: relative;
+  .search_tab {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 100;
+  }
+}
+/deep/ .el-carousel__arrow {
+  background: rgba(38, 44, 51, 0.29);
+}
+/deep/ .el-carousel__arrow:hover {
+  background: rgba(58 ,63 ,69 , .62);
+}
 </style>
