2aa157d19222cf0d679213f406d13bafc29f44d7
luoshuai990529 <1029113525@qq.com>
2020-08-30 11:52:07 +0800

    提交订单功能完成

---
+++ pages/air/order.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main">
+        <OrderForm :data="infoData" />
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import OrderForm from "@/components/air/orderForm.vue";
+export default {
+  data() {
+    return {
+      // 机票信息
+      infoData: {
+        insurances: [], // 初始化保险数据
+      },
+    };
+  },
+  mounted() {
+    const { query } = this.$route;
+    this.$axios({
+      url: `airs/${query.id}`,
+      params: {
+        seat_xid: query.seat_xid,
+      },
+    }).then((res) => {
+      this.infoData = res.data;
+    });
+  },
+  components: {
+    OrderForm,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
